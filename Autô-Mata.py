4cfad7ffe70f08755dd035a6643da7d47f47ed82
Thiago4596 <116847445+Thiago4596@users.noreply.github.com>
2025-02-22 09:50:38 -0300

    Add files via upload

--- Autô-Mata.py
+++ Autô-Mata.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 from Ferramentas.ferramentas import Ferramentas
+from Ferramentas.limpeza_windows import LimpezaArquivos
 import sys
 import threading
 import subprocess
@@ -9,9 +10,17 @@ import os
 
 # Função para verificar se o script está rodando como administrador
 def verificar_admin():
+    """
+    Verifica se o script está rodando como administrador.
+
+    Retorna:
+        bool: True se o script estiver rodando como administrador, False caso contrário.
+    """
     try:
+        # Tenta verificar se o script está rodando como administrador
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
+        # Se não for possível verificar, retorna False
         return False
 
 # Se não estiver rodando como administrador, reinicia como admin
@@ -57,41 +66,73 @@ def executar_funcao(funcaopy):
 
 # Criando a janela principal
 janela = Tk()
+guias = ttk.Notebook(janela)
+guias.pack(expand=True, fill='both')
 ferramentas = Ferramentas()
+limpeza = LimpezaArquivos()
 
-janela.title("Autô-Mata V: 2.0")
+ferramentas_aba = ttk.Frame(guias)
+ferramentas_automatizadas = ttk.Frame(guias)
+
+guias.add(ferramentas_aba, text="Ferramentas")
+guias.add(ferramentas_automatizadas, text="Ferramentas Automaticas")
+
+janela.title("Autô-Mata V: 2.1")
 icone_path = os.path.join(os.path.dirname(__file__), "Ferramentas", "icone.ico")
 
 # Definir o ícone
 janela.iconbitmap(icone_path)
-janela.geometry("500x400")
+#janela.geometry("500x600")
+janela.geometry()
+janela.resizable(False, False)
 janela.config(bg="#95a5a6")
 
 style = ttk.Style()
 style.theme_use("clam")
 
-# Criando botões para chamar as funções do script Ferramentas.ferramentas
-btn1 = ttk.Button(janela, text="Verificação de disco (Chkdsk)", command=lambda: ferramentas.check_disk())
-btn1.pack(pady=10, padx=20, fill='x')
+# Criando botões para chamar as funções do script Ferramentas.ferramentas na aba Ferramentas
+btn1 = ttk.Button(ferramentas_aba, text="Verificação de disco (Chkdsk)", command=lambda: ferramentas.check_disk())
+btn1.pack(pady=5, padx=10, fill='x')
 
-btn2 = ttk.Button(janela, text="Ferramenta de Problemas (DISM)", command=lambda: ferramentas.dism())
-btn2.pack(pady=10, padx=20, fill='x')
+btn2 = ttk.Button(ferramentas_aba, text="Ferramenta de Problemas (DISM)", command=lambda: ferramentas.dism())
+btn2.pack(pady=5, padx=10, fill='x')
 
-btn3 = ttk.Button(janela, text="Reparo do Sistema (SFC /SCANNOW)", command=lambda: ferramentas.scannow())
-btn3.pack(pady=10, padx=20, fill='x')
+btn3 = ttk.Button(ferramentas_aba, text="Reparo do Sistema (SFC /SCANNOW)", command=lambda: ferramentas.scannow())
+btn3.pack(pady=5, padx=10, fill='x')
 
-btn4 = ttk.Button(janela, text="Limpar Navegadores", command=lambda: ferramentas.limpeza_navegadores())
-btn4.pack(pady=10, padx=20, fill='x')
+btn4 = ttk.Button(ferramentas_aba, text="Limpar Navegadores", command=lambda: ferramentas.limpeza_navegadores())
+btn4.pack(pady=5, padx=10, fill='x')
 
-btn5 = ttk.Button(janela, text="Verificar Ativação do Windows (SLMGR)", command=lambda: ferramentas.windows_active())
-btn5.pack(pady=10, padx=20, fill='x')
+btn5 = ttk.Button(ferramentas_aba, text="Verificar Ativação do Windows (SLMGR)", command=lambda: ferramentas.windows_active())
+btn5.pack(pady=5, padx=10, fill='x')
 
-btn6 = ttk.Button(janela, text="Limpeza de DNS (FlushDNS)", command=lambda: ferramentas.limpeza_dns())
-btn6.pack(pady=10, padx=20, fill='x')
+btn6 = ttk.Button(ferramentas_aba, text="Limpeza de DNS (FlushDNS)", command=lambda: ferramentas.limpeza_dns())
+btn6.pack(pady=5, padx=10, fill='x')
+
+btn7 = ttk.Button(ferramentas_aba, text="Verificação de memória", command=lambda: ferramentas.verificacao_de_memoria())
+btn7.pack(pady=5, padx=10, fill='x')
+
+btn8 = ttk.Button(ferramentas_aba, text="Limpeza de disco", command=lambda: ferramentas.limpeza_de_disco())
+btn8.pack(pady=5, padx=10, fill='x')
+
+btn9 = ttk.Button(ferramentas_aba, text="Limpar a pasta Recent, Temp e Prefetch", command=lambda: limpeza.limpar_recent_temp_prefetch())
+btn9.pack(pady=5, padx=10, fill='x')
+
+btn10 = ttk.Button(ferramentas_aba, text="Service.msc", command=lambda: ferramentas.services())
+btn10.pack(pady=5, padx=10, fill='x')
+
+btn11 = ttk.Button(ferramentas_aba, text="Msconfig", command=lambda: ferramentas.msconfig())
+btn11.pack(pady=5, padx=10, fill='x')
+
+btn11 = ttk.Button(ferramentas_aba, text="Trim", command=lambda: ferramentas.trim())
+btn11.pack(pady=5, padx=10, fill='x')
 
 # Caixa de texto para exibir a saída do terminal
-caixa_texto = Text(janela, height=10, width=70)
-caixa_texto.pack(pady=10)
+caixa_texto = Text(ferramentas_aba, height=5, width=70)
+#caixa_texto.config(state = DISABLED)   
+caixa_texto.pack(pady=5)
+
+
 
 # Redireciona a saída do terminal para a caixa de texto
 sys.stdout = Redirecionador(caixa_texto)
